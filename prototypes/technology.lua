aadf84f5d1b51b38f5efcb7668f8c6c1704f16ce
Patrick PIGNOL <59389130+PatrickPIGNOL@users.noreply.github.com>
2024-08-31 06:09:27 +0200

    update 1.1.43

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -65,34 +65,6 @@ local vTechnologies = {
   },
   {
     type = "technology",
-    name = "logistics",
-    icon_size = 256, icon_mipmaps = 4,
-    icon = "__base__/graphics/technology/logistics-1.png",
-    effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = "underground-belt"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "splitter"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "loader"
-      }
-    },
-    unit =
-    {
-      count = 20,
-      ingredients = {{"automation-science-pack", 1}},
-      time = 15
-    },
-    order = "a-f-a"
-  },
-  {
-    type = "technology",
     name = "logistic-science-pack",
     localised_name = {"technology-name.logistic-science-pack"},
     localised_description = {"technology-description.logistic-science-pack"},
@@ -1948,4 +1920,129 @@ if settings.startup["SE-vanilla-science-tree-reset-activated"].value then
     do
         mSetTechnology(vTechnology);
     end
+end
+
+if mods["deadlock-beltboxes-loaders"] then
+  local vLogisiticsTechnologies = {
+    {
+      type = "technology",
+      name = "logistics",
+      icon_size = 256, icon_mipmaps = 4,
+      icon = "__base__/graphics/technology/logistics-1.png",
+      effects =
+      {
+        {
+          type = "unlock-recipe",
+          recipe = "underground-belt"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "splitter"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "loader"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "transport-belt-loader"
+        }
+      },
+      unit =
+      {
+        count = 20,
+        ingredients = {{"automation-science-pack", 1}},
+        time = 15
+      },
+      order = "a-f-a"
+    },
+    {
+      type = "technology",
+      name = "logistics-2",
+      icon_size = 256, icon_mipmaps = 4,
+      icon = "__base__/graphics/technology/logistics-2.png",
+      effects =
+      {
+        {
+          type = "unlock-recipe",
+          recipe = "fast-transport-belt"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "fast-underground-belt"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "fast-splitter"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "fast-loader"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "fast-transport-belt-loader"
+        }
+      },
+      prerequisites = {"logistics", "logistic-science-pack"},
+      unit =
+      {
+        count = 200,
+        ingredients =
+        {
+          {"automation-science-pack", 1},
+          {"logistic-science-pack", 1}
+        },
+        time = 30
+      },
+      order = "a-f-b"
+    }, 
+    {
+      type = "technology",
+      name = "logistics-3",
+      icon_size = 256, icon_mipmaps = 4,
+      icon = "__base__/graphics/technology/logistics-3.png",
+      effects =
+      {
+        {
+          type = "unlock-recipe",
+          recipe = "express-transport-belt"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "express-underground-belt"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "express-splitter"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "express-loader"
+        },
+        {
+          type = "unlock-recipe",
+          recipe = "express-transport-belt-loader"
+        }  
+      },
+      prerequisites = {"production-science-pack", "lubricant"},
+      unit =
+      {
+        count = 300,
+        ingredients =
+        {
+          {"automation-science-pack", 1},
+          {"logistic-science-pack", 1},
+          {"chemical-science-pack", 1},
+          {"production-science-pack", 1}
+        },
+        time = 15
+      },
+      order = "a-f-c"
+    },
+  }
+  for vIndex, vLogisiticsTechnology in ipairs(vLogisiticsTechnologies) 
+  do
+      mSetTechnology(vLogisiticsTechnology);
+  end
 end
